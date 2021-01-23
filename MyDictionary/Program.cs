7d00d446168a1b533808e708618f032d4ee6b3f4
elifcansu.001@outlook.com <elifcansu.001@outlook.com>
2021-01-23 18:09:02 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> ogrenci = new Dictionary<int, string>();
+            ogrenci.Add(1, "elif");
+            ogrenci.Add(2, "murat");
+            Console.WriteLine(ogrenci.Count);
+
+            MyDictionary<int,string> ogrenci2 = new MyDictionary<int,string>();
+            ogrenci2.Add(1, "burak");
+            ogrenci2.Add(2, "cemile");
+            ogrenci2.Add(2, "nevzat");
+            Console.WriteLine(ogrenci2.Count);
+
+        }
+    }
+}
